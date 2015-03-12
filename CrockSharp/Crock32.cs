e05f963d83376edfb690517f05550f6d86f5c379
slofuno <slofurno@gmail.com>
2015-03-12 01:20:32 -0400

    whitespace

--- CrockSharp/Crock32.cs
+++ CrockSharp/Crock32.cs
@@ -32,16 +32,14 @@ namespace CrockSharp
     {
       var max = one;
       var min = two;
-
       if (one.Length != two.Length)
       {
-
         if (two.Length > one.Length)
         {
           max = two;
           min = one;
         }
-
+        
         for (var i = min.Length; i < max.Length; i++)
         {
           if (max[i] != 0)
@@ -50,7 +48,6 @@ namespace CrockSharp
             return false;
           }
         }
-
       }
 
       for (var i = 0; i < min.Length; i++)
@@ -60,7 +57,6 @@ namespace CrockSharp
           return false;
         }
       }
-
       return true;
     }
 
@@ -96,105 +92,73 @@ namespace CrockSharp
       {
         //throw exception
       }
-
       return b;
     }
 
     public unsafe static byte[] Decode(string input)
     {
-
       var len = input.Length;
       var rem = len % 8;
       if (rem > 0)
       {
         char[] zeroes = Enumerable.Repeat('0', 8 - rem).ToArray();
         input = new string(zeroes) + input;
-
       }
+
       len = input.Length;
       var bytecount = (len * 5) / 8;
-
       var bytes = new byte[bytecount];
       var count = len - 8;
 
       fixed (byte* start = &bytes[0])
       {
         byte* current = start;
-
-
-
         while (count >= 0)
         {
           ulong temp = 0;
           ulong* chunk = (ulong*)current;
-
+          
           for (var i = 0; i < 8; i++)
           {
             var nextchar = input[count + i];
             temp <<= 5;
             temp |= testByte(nextchar);
-
-
           }
 
           *chunk |= temp;
           current += 5;
           count -= 8;
         }
-
-
       }
-
-
       return bytes;
-
-
     }
 
     public static byte getByte(char c)
     {
-
       return (byte)5;
     }
 
     public unsafe static string Encode(byte[] input)
     {
-
-
       var len = input.Length;
-      /*
-
-      */
       var rem = len % 5;
-
-
       if (rem > 0)
       {
         var zeroes = new byte[5 - rem];
         zeroes = zeroes.Select(x => (byte)0).ToArray();
         input = input.Concat(zeroes).ToArray();
       }
-
       len = input.Length;
       var wordcount = (8 * len) / 5;
-
       var words = new uint[wordcount];
-
-
       var count = 0;
 
       fixed (byte* inputp = &input[0])
       {
-
         var start = inputp;
-
         while (count < wordcount)
         {
-
           var chunk = (ulong*)start;
-
-
-
           words[count] = (uint)*chunk & 31;
           words[count + 1] = (uint)((*chunk >> 5) & 31);
           words[count + 2] = (uint)((*chunk >> 10) & 31);
@@ -203,94 +167,19 @@ namespace CrockSharp
           words[count + 5] = (uint)((*chunk >> 25) & 31);
           words[count + 6] = (uint)((*chunk >> 30) & 31);
           words[count + 7] = (uint)((*chunk >> 35) & 31);
-
           start += 5;
           count += 8;
         }
-
       }
 
       while (wordcount > 0 && words[wordcount - 1] == 0)
       {
         wordcount--;
       }
-
 
       words = words.Take(wordcount).Reverse().ToArray();
-
-
       var output = words.Select(x => crockmap[x]).ToArray();
-
       return new string(output);
-
     }
-
-    public unsafe static string Encode2(byte[] input)
-    {
-
-
-      var len = input.Length;
-      /*
-
-      */
-      var rem = len % 5;
-
-
-      if (rem > 0)
-      {
-        var zeroes = new byte[5 - rem];
-        zeroes = zeroes.Select(x => (byte)0).ToArray();
-        input = input.Concat(zeroes).ToArray();
-      }
-
-      len = input.Length;
-      var wordcount = (8 * len) / 5;
-
-      var words = new uint[wordcount];
-
-
-      var count = 0;
-
-      fixed (byte* inputp = &input[0])
-      {
-
-        var start = inputp;
-
-        while (count < wordcount)
-        {
-
-          var chunk = (ulong*)start;
-          words[count] = (uint)*chunk & 31;
-          words[count + 1] = (uint)((*chunk >> 5) & 31);
-          words[count + 2] = (uint)((*chunk >> 10) & 31);
-          words[count + 3] = (uint)((*chunk >> 15) & 31);
-          words[count + 4] = (uint)((*chunk >> 20) & 31);
-          words[count + 5] = (uint)((*chunk >> 25) & 31);
-          words[count + 6] = (uint)((*chunk >> 30) & 31);
-          words[count + 7] = (uint)((*chunk >> 35) & 31);
-
-          start += 5;
-          count += 8;
-        }
-
-      }
-
-      while (wordcount > 0 && words[wordcount - 1] == 0)
-      {
-        wordcount--;
-      }
-
-      if (wordcount < words.Length)
-      {
-        words = words.Take(wordcount).Reverse().ToArray();
-      }
-
-      var output = words.Select(x => crockmap[x]).ToArray();
-
-      return new string(output);
-
-    }
-
-
   }
 }
